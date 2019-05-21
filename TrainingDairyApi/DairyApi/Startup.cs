28e68b9e71d728a702f08d2608cc9ff21d1d84c6
Alexander Naumov <aleksandr.naumov@fortegrp.net>
2019-05-21 23:44:21 +0300

    added users; added db context; added user migration;

--- TrainingDairyApi/DairyApi/Startup.cs
+++ TrainingDairyApi/DairyApi/Startup.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DairyApi
 {
@@ -24,6 +29,21 @@ namespace DairyApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddCors();
+
+            services.AddDbContext<DairyContext>(c => c.UseSqlServer(Configuration.GetConnectionString("EFConnectionString")));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +54,8 @@ namespace DairyApi
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
---
+++ TrainingDairyApi/Domain/DairyContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class DairyContext : IdentityDbContext<AppUser>
+    {
+        public DairyContext() : base()
+        {
+
+        }
+
+        public DairyContext(DbContextOptions<DairyContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public new DbSet<AppUser> Users { get; set; }
+    }
+}
