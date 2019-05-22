408f967017842830d77deef282bd9390e6cf4003
Alexander Naumov <aleksandr.naumov@fortegrp.net>
2019-05-22 11:54:41 +0300

    added training service interface

---
+++ TrainingDairyApi/Domain/Dto/TrainingDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dto
+{
+    public class TrainingDto
+    {
+        public int ID { get; set; }
+        public int SportsmanID { get; set; }
+        public TrainingType TrainingType { get; set; }
+        public DateTime TrainingStart { get; set; }
+        public int TrainingTimeInSeconds { get; set; }
+        public short AmountOfSets { get; set; }
+    }
+}
---
+++ TrainingDairyApi/Domain/Interfaces/ITrainingService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Dto;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface ITrainingService
+    {
+        TrainingDto GetById(int id);
+        TrainingDto Create(TrainingDto newTraining);
+        TrainingDto Edit(TrainingDto training);
+        void Delete(int id);
+        IEnumerable<TrainingDto> GetAll();
+        IEnumerable<TrainingDto> GetAllForUser(int userId);
+    }
+}
